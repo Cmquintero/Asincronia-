ff72fdb032e03ff74a55dfaa199a00a19ffd4507
cmquintero <cmquinterot@ufpso.edu.co>
2024-06-07 18:10:20 -0500

    two commit

---
+++ src/components/vue/loadData.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+const users = ref([])
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            users.value = data
+        })
+
+
+}
+</script>
+<template>
+    <div v-for="user in users"
+    :key="user.id" 
+    class=" bg-slate-300 mb-1 mt-4 text-center rounded-xl p-2 max-w-60 mx-auto " >{{ user.name }}</div>
+    <button @click="loadData" class=" rounded-xl  border bg-emerald-400  flex p-2 max-w-60 mx-auto mt-12">Cargar info</button>
+</template>
